ecd01cc0eacd7d8c599ae7a80c12ce719ff0ad46
ema18787 <emanueljtc@gmail.com>
2018-11-04 15:10:20 -0400

    reduciendo las funciones

--- public/js/script.js
+++ public/js/script.js
@@ -1,104 +1,58 @@
-var options = document.getElementsByClassName("options");
-var arrowUp = document.getElementsByClassName("fa-angle-double-up");
-var arrowDown = document.getElementsByClassName("fa-angle-double-down");
-var line = document.getElementsByClassName("line");
-var btnTeclado = document.getElementsByClassName("btn-teclado");
-var btnCombo = document.getElementsByClassName("btn-combo");
-var teclado = document.getElementsByClassName("teclado");
-var combo = document.getElementsByClassName("combo");
+let options = document.getElementById("options");
+let arrowUp = document.getElementById("arrowUp");
+let arrowDown = document.getElementById("arrowDown");
+let line = document.getElementById("line");
+let btnTeclado = document.getElementById("buttonTeclado");
+let btnCombo = document.getElementById("buttonCombo");
+let teclado = document.getElementById("teclado");
+let combo = document.getElementById("combo");
 
 
 // Function Toogle
 function toogleUp(){
-
-  for (var i= 0; i<options.length; i++){
-    options[i].classList.replace("ocultar", "mostrar");
+  options.classList.replace("ocultar", "mostrar");
+  arrowUp.classList.replace("mostrar", "ocultar");
+  arrowDown.classList.replace("ocultar", "mostrar");
+  
+  if (btnTeclado.classList.contains('ocultar')) {
+      line.classList.remove("mostrar");
+      line.classList.add("ocultar");
   }
-  for (var i = 0; i < arrowUp.length; i++) {
-    arrowUp[i].classList.replace("mostrar", "ocultar");
-  }
-  for (var i = 0; i < arrowDown.length; i++) {
-    arrowDown[i].classList.replace("ocultar", "mostrar");
-  }
-  if (document.getElementById('tecladoId').classList.contains('ocultar')) {
-    for (var i = 0; i < line.length; i++) {
-      line[i].classList.remove("mostrar");
-      line[i].classList.add("ocultar");
-    }
-  }
-  for (var i = 0; i < btnTeclado.length; i++) {
-    btnTeclado[i].disabled = false;
-  }
-  for (var i = 0; i < btnCombo.length; i++) {
-    btnCombo[i].disabled = false;
-  }
+  
+  btnTeclado.disabled = false;
+  btnCombo.disabled = false;
 }
 function toogleDown() {
-  
-  for (var i = 0; i < options.length; i++) {
-    options[i].classList.replace("mostrar", "ocultar");
-  }
-  for (var i = 0; i < arrowUp.length; i++) {
-    arrowUp[i].classList.replace("ocultar", "mostrar");
-  }
-  for (var i = 0; i < arrowDown.length; i++) {
-    arrowDown[i].classList.replace("mostrar", "ocultar");
-  }
-  for (var i = 0; i < line.length; i++) {
-    line[i].classList.remove("ocultar");
-  }
-  for (var i = 0; i < btnTeclado.length; i++) {
-    btnTeclado[i].disabled = true;
-  }
-  for (var i = 0; i < btnCombo.length; i++) {
-    btnCombo[i].disabled = true;
-  }
+  options.classList.replace("mostrar", "ocultar");
+  arrowUp.classList.replace("ocultar", "mostrar");
+  arrowDown.classList.replace("mostrar", "ocultar");
+  line.classList.remove("ocultar");
+  btnTeclado.disabled = true;
+  btnCombo[i].disabled = true;
 }
 
 //Function teclado
 
 function f_teclado(){
-  for (var i = 0; i < teclado.length; i++) {
-    teclado[i].classList.remove("ocultar");
-  }
-  for (var i = 0; i < combo.length; i++) {
-    combo[i].classList.add("ocultar");
-  }
-  for (var i = 0; i < btnTeclado.length; i++) {
-    btnTeclado[i].classList.remove("mostrar");
-    btnTeclado[i].classList.add("ocultar");
-  }
-  for (var i = 0; i < btnCombo.length; i++) {
-    btnCombo[i].classList.remove("ocultar");
-    btnCombo[i].classList.add("mostrar_null");
-  }
-  for (var i = 0; i < line.length; i++) {
-    line[i].classList.add("ocultar");
-  }
+  teclado.classList.remove("ocultar");
+  combo.classList.add("ocultar");
+  btnTeclado.classList.remove("mostrar");
+  btnTeclado.classList.add("ocultar");
+  btnCombo.classList.remove("ocultar");
+  btnCombo.classList.add("mostrar_null");
+  line.classList.add("ocultar");
 }
 
 // Function Combo
 
 function f_combo(){
-
-  for (var i = 0; i < teclado.length; i++) {
-    teclado[i].classList.add("ocultar");
-    teclado[i].classList.remove("mostrar");
-  }
-  for (var i = 0; i < combo.length; i++) {
-    combo[i].classList.remove("ocultar");
-    combo[i].classList.add("mostrar");
-  }
-  for (var i = 0; i < btnCombo.length; i++) {
-    btnCombo[i].classList.remove("mostrar_null");
-    btnCombo[i].classList.add("ocultar");
-  }
-  
-  for (var i = 0; i < btnTeclado.length; i++) {
-    btnTeclado[i].classList.remove("ocultar");
-    btnTeclado[i].classList.add("mostrar_null");
-  }
-  for (var i = 0; i < line.length; i++) {
-    line[i].classList.remove("ocultar");
-  }
+  teclado.classList.add("ocultar");
+  teclado.classList.remove("mostrar");
+  combo.classList.remove("ocultar");
+  combo.classList.add("mostrar");
+  btnCombo.classList.remove("mostrar_null");
+  btnCombo.classList.add("ocultar");
+  btnTeclado.classList.remove("ocultar");
+  btnTeclado.classList.add("mostrar_null");
+  line.classList.remove("ocultar");
 }
